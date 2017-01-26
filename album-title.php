5b7ffa1e39ec1fcf9db539d43763b5234847f98d
Michael Roe <Mike@Michaels-MacBook-Pro.local>
2017-01-26 15:56:54 -0500

    first commit

---
+++ album-title.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Album Titles</title>
+</head>
+<body>
+
+<?php
+
+// connect to db
+$conn = new PDO('mysql:host=localhost;dbname=', 'root', 'TiggerKitty21');
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // helps with debugging db errors
+
+// write an sql query to select the album titles
+$sql = "SELECT title FROM albums";
+
+// execute the query and store the results
+$cmd = $conn->prepare($sql);
+$cmd->execute();
+$albums = $cmd->fetchAll(); // $albums is the whole collection of data from the query
+
+// loop through the data - $album is the current item in the list of $albums
+
+foreach($albums as $album) {
+    // display the titles using echo
+    echo $album['title'] . '<br />';
+}
+
+
+
+// disconnect
+$conn = null;
+?>
+
+
+
+</body>
+</html>
